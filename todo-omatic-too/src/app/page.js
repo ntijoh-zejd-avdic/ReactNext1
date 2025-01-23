0cc5400008238f91a84a7e195ed7a4525d265f19
ntijoh-zejd-avdic <zejd.avdic@elev.ga.ntig.se>
2025-01-23 15:36:02 +0100

    Button added

--- todo-omatic-too/src/app/page.js
+++ todo-omatic-too/src/app/page.js
@@ -12,6 +12,9 @@ export default function Home() {
           height={38}
           priority
         />
+
+        <button class="bg-sky-500 hover:bg-sky-700 ...">Fart Man</button>
+
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
             Get started by editing{" "}
