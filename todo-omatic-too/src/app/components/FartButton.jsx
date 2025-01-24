4f0791b3219ebce13550d2550edb7a48463e0f1c
ntijoh-zejd-avdic <zejd.avdic@elev.ga.ntig.se>
2025-01-24 09:21:38 +0100

    Deleted default code, created FartButton component, FartButton works

---
+++ todo-omatic-too/src/app/components/FartButton.jsx
@@ -0,0 +1,11 @@
+export default function FartButton() {
+    return (
+      <button
+        className="bg-sky-500 hover:bg-sky-700 text-white font-bold py-2 px-4 rounded"
+        onClick={() => alert("Fart Man is here!")}
+      >
+        Fart Man
+      </button>
+    );
+  }
+  
